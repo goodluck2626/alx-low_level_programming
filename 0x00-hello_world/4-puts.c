4ddb37bbef61176ee8a0674249608bdfbd2ce342
goodluck2626 <nkemgoodluck5@gmail.com>
2023-09-10 07:50:16 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,11 +4,12 @@
  * function - use puts to 
  * print to standard output
  *
+ * Return 0 (Success)
  */
 
-void main()
+int  main(void)
 {
 	puts("Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
